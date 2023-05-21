d1f1e381638a9b0368da11af3f9c6452479baf1a
max <max.nick.demax@gmail.com>
2023-05-21 09:32:33 +0300

    add Thymeleaf

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,13 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
@@ -16,6 +21,20 @@ public class App {
     }
 
 
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
     public static void main(String[] args) {
         LOGGER.info("Start");
         App.getApp().start(getPort());
@@ -23,7 +42,16 @@ public class App {
 
     public static Javalin getApp() {
         LOGGER.debug("Get Javalin app.");
-        return Javalin.create()
-                .get("/", ctx -> ctx.result("Hello World"));
+        return Javalin.create(config -> {
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
+        }).get("/", ctx -> ctx.render("index.html"));
+    }
+
+    private static boolean isProduction() {
+        return false;
     }
 }
