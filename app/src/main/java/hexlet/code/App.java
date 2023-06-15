b338e7afbf6b628efa4fc9d4f48b347aced01e2f
max <max.nick.demax@gmail.com>
2023-06-15 06:48:32 +0300

    enable webjars

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -56,6 +56,7 @@ public class App {
             if (!isProduction()) {
                 config.plugins.enableDevLogging();
             }
+            config.staticFiles.enableWebjars();
             JavalinThymeleaf.init(getTemplateEngine());
         });
         addRoutes(app);
